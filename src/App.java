bb5c705a8a8ce997621ccda4fc89093392f61be6
Noah Söng <noah.song@elev.ga.ntig.se>
2023-09-04 09:28:32 +0200

    lagt till 3 andra operatorer (-, * och /)

--- src/App.java
+++ src/App.java
@@ -3,7 +3,13 @@ public class App {
         int tal1 = 10;
         int tal2 = 20;
         int summa = tal1+tal2;
-        System.out.println("Summan av tal1 + tal 2 ="+summa);
+        int differens = tal2-tal1;
+        int produkt = tal1*tal2;
+        int kvot = tal2/tal1;
+        System.out.println("Summan av tal1 + tal 2 ="+summa);   
+        System.out.println("Differensen av tal2 - tal 1 ="+differens);   
+        System.out.println("Produkten av tal1 * tal 2 ="+produkt);   
+        System.out.println("Kvoten av tal1 / tal 2 ="+kvot);   
         
 
     }
