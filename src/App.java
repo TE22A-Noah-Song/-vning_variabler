796f7333432abb046c0f8be33022344ae5123b97
Noah Söng <noah.song@elev.ga.ntig.se>
2023-09-04 09:24:53 +0200

    Lagt till addition operator

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int tal1 = 10;
+        int tal2 = 20;
+        int summa = tal1+tal2;
+        System.out.println("Summan av tal1 + tal 2 ="+summa);
+        
+
     }
 }
