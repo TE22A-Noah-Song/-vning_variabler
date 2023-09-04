a17d49735bc98429a6956d350e49f2b847edd871
Noah Söng <noah.song@elev.ga.ntig.se>
2023-09-04 09:33:28 +0200

    lagt till modulo

--- src/App.java
+++ src/App.java
@@ -10,7 +10,10 @@ public class App {
         System.out.println("Differensen av tal2 - tal 1 ="+differens);   
         System.out.println("Produkten av tal1 * tal 2 ="+produkt);   
         System.out.println("Kvoten av tal1 / tal 2 ="+kvot);   
-        
 
+        int rest1 = 15%2;
+        int rest2 = 8%3;
+        System.out.println("Resten av 15/2 är "+rest1);
+        System.out.println("Resten av 8/3 är "+rest2);
     }
 }
